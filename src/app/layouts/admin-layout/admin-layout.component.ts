2961e9407d077c2ad741afdea96a5f4fae0dba31
d4160 <d4160@outlook.com>
2021-02-03 13:11:56 -0500

    Added compatibility for heroku deploy.

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -69,6 +69,7 @@ export class AdminLayoutComponent implements OnInit {
 
       }
 
+      // prevent default close panel
       $('.fixed-plugin a').click(function(event){
         // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
           if($(this).hasClass('switch-trigger')){
@@ -81,6 +82,7 @@ export class AdminLayoutComponent implements OnInit {
           }
       });
 
+      // color switch
       $('.fixed-plugin .badge').click(function(){
           let $full_page_background = $('.full-page-background');
 
